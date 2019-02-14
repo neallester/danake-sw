ac96cc694d3df0c02ff6490878f008286ddae5bd
Neal Lester <neal@3dsafety.com>
2019-02-13 22:29:12 -0600

    Added JSONEquality to main target for inclusion in SampleUsage.

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "danake",
-            dependencies: ["PromiseKit"]),
+            dependencies: ["PromiseKit", "JSONEquality"]),
         .testTarget(
             name: "danakeTests",
             dependencies: ["danake", "JSONEquality"]),
